ca1172c61c6f9b3488c03a685ce518945928e57c
David Brazda <davidbrazda61@gmail.com>
2024-02-27 08:15:07 +0700

    batchprofit/batchcount columns hidden from archiverunners gui

--- v2realbot/common/db.py
+++ v2realbot/common/db.py
@@ -117,8 +117,8 @@ def row_to_runarchiveview(row: dict) -> RunArchiveView:
         end_positions=int(row['end_positions']),
         end_positions_avgp=float(row['end_positions_avgp']),
         metrics=orjson.loads(row['metrics']) if row['metrics'] else None,
-        batch_profit=int(row['batch_profit']) if row['batch_profit'] else 0,
-        batch_count=int(row['batch_count']) if row['batch_count'] else 0,
+        batch_profit=int(row['batch_profit']) if row['batch_profit'] and row['batch_id'] else 0,
+        batch_count=int(row['batch_count']) if row['batch_count'] and row['batch_id'] else 0,
     )
     return a
 
